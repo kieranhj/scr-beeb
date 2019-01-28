fb6642ef8a5eb0141e5609e12fe578733a0dbade
kieranhj <kieran@kconnell.co.uk>
2019-01-28 09:50:17 +0000

    Added credits screen to build
    (Credits screen not used yet)
    Added Pucrunch of itle screen and credits screen to build

--- make.bat
+++ make.bat
@@ -3,7 +3,7 @@ set PYTHON=C:\Home\Python27\python.exe
 
 	%PYTHON% bin/png2bbc.py -o build/scr-beeb-hud.dat -m build/scr-beeb-hud-mask.dat --160 --palette 0143 --transparent-output 3 --transparent-rgb 255 0 255 ./graphics/scr-beeb-hud.png 5
 
-	%PYTHON% bin/png2bbc.py -o build/scr-beeb-header.dat --160 --palette 0143 ./graphics/scr-beeb-header.png 5
+rem	%PYTHON% bin/png2bbc.py -o build/scr-beeb-header.dat --160 --palette 0143 ./graphics/scr-beeb-header.png 5
 
 	%PYTHON% bin/png2bbc.py -o build/scr-beeb-title-screen.dat --160 ./graphics/TitleScreen_BBC.png 2
 
@@ -17,7 +17,11 @@ set PYTHON=C:\Home\Python27\python.exe
 
 	%PYTHON% bin/track_preview.py > build/track-preview.asm
 
+	%PYTHON% bin/png2bbc.py -o build/scr-beeb-credits.dat --160 ./graphics/scr-beeb-credits.png 2
+
+	bin\pucrunch.exe -d -c0 -l0x1000 "build\scr-beeb-title-screen.dat" build\scr-beeb-title-screen.pu
 	bin\pucrunch.exe -d -c0 -l0x1000 "build\scr-beeb-menu.dat" build\scr-beeb-menu.pu
+	bin\pucrunch.exe -d -c0 -l0x1000 "build\scr-beeb-credits.dat" build\scr-beeb-credits.pu
 
 ..\beebasm\beebasm.exe -i scr-beeb.asm -do scr-beeb.ssd -boot Loader -v > compile.txt
 %PYTHON% bin\crc32.py scr-beeb.ssd
